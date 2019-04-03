1c602d1216bd14f9aea08ae59448adf49676a0a6
William <williamferk@gmail.com>
2019-04-03 12:07:11 +0200

    fiat lux

---
+++ app/src/main/java/fk/home/webviewexp/MainActivity.kt
@@ -0,0 +1,51 @@
+package fk.home.webviewexp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        setupwebview()
+        loadWebview()
+        fab.setOnClickListener {
+            loadWebview()
+        }
+    }
+
+    private fun setupwebview() {
+        webview.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+        }
+    }
+
+    private fun loadWebview() {
+        webview.loadUrl("http://192.168.0.124:4200/")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
